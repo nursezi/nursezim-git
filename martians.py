01efc02bbc5934f658ef7066543206b4915b63e0
nursezi <151495919+nursezi@users.noreply.github.com>
2023-11-26 03:46:29 +0900

    Add files via upload

---
+++ martians.py
@@ -0,0 +1,29 @@
+def find_cargo():
+    total_weight = 713
+    cargo_weights = [0, 0, 0]
+    kilometer_marks = [0, 0, 0]
+
+    while True:
+        print("\nEnter the kilometer mark for each box:")
+
+        for i in range(3):
+            kilometer_marks[i] = int(input(f"Box {i + 1}: "))
+
+        for i in range(3):
+            if cargo_weights[i] == 0 or kilometer_marks[i] == 0:
+                cargo_weights[i] = int(input(f"Enter the weight for Box {i + 1}: "))
+
+        if sum(cargo_weights) == total_weight:
+            print("\nCongratulations! You found all the boxes.")
+            break
+        else:
+            print("\nOops! The total weight is not 713. Try again.")
+
+if __name__ == "__main__":
+    print("Welcome to the Martian Cargo Finder!")
+    print("You need to enter kilometer marks and weights for each box.")
+
+    find_cargo()
+
+    print("\nThank you for helping the Martians!")
+    print("They are impressed with your skills and may contact you for an intergalactic project.")
